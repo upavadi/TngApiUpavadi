2437b0effbeb0d969d09bd9be6d6512345a593c4
Mahesh Upadhyaya <mahesh@upavadi.net>
2015-11-09 16:34:37 +0000

    added profile images to report

--- Shortcode/BirthdaysPlusOne.php
+++ Shortcode/BirthdaysPlusOne.php
@@ -10,14 +10,22 @@ class TngApiCustom_Shortcode_BirthdaysPlusOne extends Upavadi_Shortcode_Abstract
 
         $month = date('m');
         $birthdaysplusone = $this->custom->getBirthdaysPlusOne($month);
-        
-        //get and hold current user
+        // Get Media
+		$url = $this->content->getTngUrl();
+		$photos = $this->content->getTngPhotoFolder();
+		$photosPath = $url. $photos;
+		//get and hold current user
 		$currentperson = $this->content->getCurrentPersonId($person['personID']);
         $context = array(
             'month' => $month,
+			'gedcom' => $tree,
+			'personid' => $personId,
             'birthdaysplusone' => $birthdaysplusone,
             'date' => date('F Y', strtotime('+1 month')),
-			'currentperson' => $currentperson
+			'currentperson' => $currentperson,
+			'dmedia' => $dmedia,
+			'photosPath' => $photosPath,
+			'mediaPath' => $mediaPath
         );
         return $this->templates->render('birthdaysplusone.html', $context);
     }
--- Shortcode/ManniversariesPlusOne.php
+++ Shortcode/ManniversariesPlusOne.php
@@ -10,14 +10,22 @@ class TngApiCustom_Shortcode_ManniversariesPlusOne extends Upavadi_Shortcode_Abs
 
         $month = date('m');
         $manniversariesplusone = $this->custom->getMarriageAnniversariesPlusOne($month);
-        
+        // Get Media
+		$url = $this->content->getTngUrl();
+		$photos = $this->content->getTngPhotoFolder();
+		$photosPath = $url. $photos;
+		
         //get and hold current user
 		$currentperson = $this->content->getCurrentPersonId($person['personID']);
         $context = array(
             'month' => $month,
+			'gedcom' => $tree,
+			'personid' => $personId,
             'manniversariesplusone' => $manniversariesplusone,
             'date' => date('F Y', strtotime('+1 month')),
-			'currentperson' => $currentperson
+			'currentperson' => $currentperson,
+			'photosPath' => $photosPath
+			
         );
         return $this->templates->render('m-anniversariesplusone.html', $context);
     }
