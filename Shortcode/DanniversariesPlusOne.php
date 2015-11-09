d4fe06eb6e1827d111c05d7f7e67d4066d93edd6
Mahesh Upadhyaya <mahesh@upavadi.net>
2015-11-09 16:30:58 +0000

    Added profile image and age at death

--- Shortcode/DanniversariesPlusOne.php
+++ Shortcode/DanniversariesPlusOne.php
@@ -10,14 +10,21 @@ class TngApiCustom_Shortcode_DanniversariesPlusOne extends Upavadi_Shortcode_Abs
 
         $month = date('m');
         $danniversariesplusone = $this->custom->getDeathAnniversariesPlusOne($month);
-        
-        //get and hold current user
+        // Get Media
+		$url = $this->content->getTngUrl();
+		$photos = $this->content->getTngPhotoFolder();
+		$photosPath = $url. $photos;
+		//get and hold current user
 		$currentperson = $this->content->getCurrentPersonId($person['personID']);
         $context = array(
             'month' => $month,
             'danniversariesplusone' => $danniversariesplusone,
+			'gedcom' => $tree,
+			'personid' => $personId,
             'date' => date('F Y', strtotime('+1 month')),
-			'currentperson' => $currentperson
+			'currentperson' => $currentperson,
+			'photosPath' => $photosPath
+			
         );
         return $this->templates->render('d-anniversariesplusone.html', $context);
     }
--- templates/d-anniversariesplusone.html.php
+++ templates/d-anniversariesplusone.html.php
@@ -3,23 +3,62 @@ Clicking on a name takes you to the Individual's Family Page</br> Clicking on VI
 <table cellspacing="1" cellpadding="1" border="1">
 	<tbody>
 		
-			<th style="background-color: #EDEDED;">Name</th>
-			<th style="background-color: #EDEDED;">DEath Date</th>
-			<th style="background-color: #EDEDED;">Death Place</th>
-			<th style="background-color: #EDEDED;">Years  </th>
-			<th style="background-color: #EDEDED;">Relationship</th>
+			<th class="theader" style="text-align: center">Name</th>
+			<th class="theader">Date</th>
+			<th class="theader">Death Place</th>
+			<th class="theader" style="text-align: center">Years</th>
+			<th class="theader" style="text-align: center">Age at Death</th>
+			<th class="theader">Relationship</th>
+			
 			
 		
 		
 	
-<?php foreach ($danniversariesplusone as $danniversary): ?>
+		<?php 
+		$tngcontent = Upavadi_TngContent::instance();
+		foreach ($danniversariesplusone as $danniversary):
+		$tree = $danniversary['gedcom'];
+		$personId = $danniversary['personid'];
+		$name = $danniversary['firstname']. " ". $danniversary['lastname'];
+		$dmedia = $tngcontent->getDefaultMedia($personId, $tree);
+		$mediaPath = $photosPath."/". $dmedia['thumbpath'];
+		
+		//get age at death
+		if ($danniversary['birthdatetr'] !== "0000-00-00") {
+			$d_birtharray = explode("-", ($danniversary['birthdatetr']));
+			$d_birthyear = $d_birtharray[0];
+			$d_birthmonth = $d_birtharray[1];
+			$d_birthday = $d_birtharray[2];
+
+			$deatharray = explode("-", ($danniversary['deathdatetr']));
+			$deathyear = $deatharray[0];
+			$deathmonth = $deatharray[1];
+			$deathday = $deatharray[2];
+			$setBirthdate = new DateTime();
+			$setBirthdate->setDate($d_birthyear, $d_birthmonth, $d_birthday);
+			
+			$setDeathdate = new DateTime();
+			$setDeathdate->setDate($deathyear, $deathmonth, $deathday);
+			$setBirthdate->format('c') . "<br / >\n";
+			$setDeathdate->format('c') . "<br / >\n";
+			$i = $setBirthdate->diff($setDeathdate);
+			$i->format("%Y");
+			$ageAtDeath = $i->format("%Y");
+			}	else { 	$ageAtDeath = "";
+		}	
+		?>
 		<tr>
-			<td><a href="/family/?personId=<?php echo $danniversary['personid'];?>">
-			<?php echo $danniversary['firstname']; ?><?php echo $danniversary['lastname']; ?></a></td>
+			<td style="text-align: center"><div>
+			<?php if ($dmedia['thumbpath']) { ?>
+			<img src="<?php 
+			echo "$mediaPath";  ?>" border='1' height='50' border-color='#000000'/> <?php } ?><br /> 
+			
+			<a href="/family/?personId=<?php echo $danniversary['personid'];?>">
+			<?php echo $name; ?></a></div></td>
 			<td><?php echo $danniversary['deathdate']; ?></td>
 			<td><?php echo $danniversary['deathplace']; ?></td>
-			<td><?php echo $danniversary['Years']; ?></td>
-			
+			<td style="text-align: center"><?php echo $danniversary['Years']; ?></td>
+			<td style="text-align: center"><?php echo $ageAtDeath; ?></td>
 			<td><a href="../genealogy/relationship.php?altprimarypersonID=&savedpersonID=&secondpersonID=<?php echo $danniversary['personid'];?>&maxrels=2&disallowspouses=0&generations=15&tree=upavadi_1&primarypersonID=<?php echo $currentperson; ?>"><?php echo "View"?></td>
 			
 		</tr>
