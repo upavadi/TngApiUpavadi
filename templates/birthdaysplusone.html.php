b903ff6758e1fa933b1b6d7917bd3fc42a7b711b
Mahesh Upadhyaya <mahesh@upavadi.net>
2015-11-09 16:35:31 +0000

    added profile images to report

--- templates/birthdaysplusone.html.php
+++ templates/birthdaysplusone.html.php
@@ -1,25 +1,38 @@
-
+<?php
+//$tngcustom = TngApiCustom_TngCustom()::instance()->init();
+$tngcontent = Upavadi_TngContent::instance();
+?>
 <h2>Birthdays for <?php echo $date ?></h2>
 Clicking on a name takes you to the Individual's Family Page
-</br> Clicking on VIEW will show your relationship to the individual (Blood relationships only)								
-<table cellspacing="1" cellpadding="1" border="1">
+</br> Clicking on VIEW will show your relationship to the individual (Blood relationshipsbbbbb only)								
+<table>
 	<tbody>
-			<th style="background-color: #EDEDED;">Name</th>
+			<th style="background-color: #EDEDED; text-align: center;">Name</th>
 			<th style="background-color: #EDEDED;">Date</th>
 			<th style="background-color: #EDEDED;">Birth Place</th>
 			<th style="background-color: #EDEDED;">Age</th>
 			<th style="background-color: #EDEDED;">Relationship</th>
 	
-	<?php foreach ($birthdaysplusone as $birthday): ?>
+	<?php foreach ($birthdaysplusone as $birthday): 
+	$tree = $birthday['gedcom'];
+	$personId = $birthday['personid'];
+	$dmedia = $tngcontent->getDefaultMedia($personId, $tree);
+	$mediaPath = $photosPath."/". $dmedia['thumbpath'];
+	?>
 		<tr>
-			<td><a href="/family/?personId=<?php echo $birthday['personid'];?>">
-			<?php echo $birthday['firstname']." "; ?><?php echo $birthday['lastname']; ?></a></td>
-			<td><?php echo $birthday['birthdate']; ?></td>
+			<td style="text-align: center"><div>
+			<?php if ($dmedia['thumbpath']) { ?>
+			<img src="<?php 
+			echo "$mediaPath";  ?>" border='1' height='50' border-color='#000000'/> <?php } ?><br /> 
+			<a href="/family/?personId=<?php echo $birthday['personid'];?>&amp;tree=<?php echo $tree; ?>">
+            <?php echo $birthday['firstname']. " "; ?><?php echo $birthday['lastname']; ?></a></div></td>
+            <td><?php echo $birthday['birthdate']; ?></td>
 			<td><?php echo $birthday['birthplace']; ?></td>
 			<td><?php echo $birthday['Age']; ?></td>
 			<td><a href="../genealogy/relationship.php?altprimarypersonID=&savedpersonID=&secondpersonID=<?php echo $birthday['personid'];?>&maxrels=2&disallowspouses=0&generations=15&tree=upavadi_1&primarypersonID=<?php echo $currentperson; ?>"><?php echo "View"?></td>
 			
 		</tr>
+		
 <?php endforeach; ?>
 	</tbody>
 </table>
--- templates/m-anniversariesplusone.html.php
+++ templates/m-anniversariesplusone.html.php
@@ -2,18 +2,43 @@
 Clicking on a name takes you to the Individual's Family Page.</br>
 <table cellspacing="1" cellpadding="1" border="1">
 	<tbody>
-			<th style="background-color: #EDEDED;">Husband</th>
-			<th style="background-color: #EDEDED;">Wife</th>
+			<th style="background-color: #EDEDED; text-align: center;">Husband</th>
+			<th style="background-color: #EDEDED; text-align: center;">Wife</th>
 			<th style="background-color: #EDEDED;">Date</th>
 			<th style="background-color: #EDEDED;">Place</th>
-			<th style="background-color: #EDEDED;">Years</th>
+			<th style="background-color: #EDEDED; text-align: center;">Years</th>
 	
-<?php foreach ($manniversariesplusone as $manniversary):?>
+	<?php 
+	
+	$tngcontent = Upavadi_tngcontent::instance()->init();
+	
+		foreach ($manniversariesplusone as $manniversary):
+		$tree = $manniversary['gedcom'];
+		$personId1 = $manniversary['personid1'];
+		$personId2 = $manniversary['personid2'];
+		$dmedia1 = $tngcontent->getDefaultMedia($personId1, $tree);
+		$dmedia2 = $tngcontent->getDefaultMedia($personId2, $tree);
+		$mediaPath1 = $photosPath."/". $dmedia1['thumbpath'];
+		$mediaPath2 = $photosPath."/". $dmedia2['thumbpath'];
+	
+	?>
 		<tr>
-			<td><a href="/family/?personId=<?php echo $manniversary['personid1'];?>">
-			<?php echo $manniversary['firstname1']; ?><?php echo $manniversary['lastname1']; ?></a></td>
-			<td><a href="/family/?personId=<?php echo $manniversary['personid2'];?>">
-			<?php echo $manniversary['firstname2']; ?><?php echo $manniversary['lastname2']; ?></a></td>
+			<td style="text-align: center"><div>
+			<?php if ($dmedia1['thumbpath']) { ?>
+			<img src="<?php 
+			echo "$mediaPath1";  ?>" border='1' height='50' border-color='#000000'/> <?php } ?>
+			<br />
+			<a href="/family/?personId=<?php echo $manniversary['personid1'];?>">
+			<?php echo $manniversary['firstname1']; ?><?php echo $manniversary['lastname1']; ?></a></div></td>
+			
+			<td style="text-align: center"><div>
+			<?php if ($dmedia2['thumbpath']) { ?>
+			<img src="<?php 
+			echo "$mediaPath2";  ?>" border='1' height='50' border-color='#000000'/> <?php } ?>
+			<br />
+			
+			<a href="/family/?personId=<?php echo $manniversary['personid2'];?>">
+			<?php echo $manniversary['firstname2']; ?><?php echo $manniversary['lastname2']; ?></a></div></td>
 				
 			<td><?php echo $manniversary['marrdate']; ?></a></td>
 			
