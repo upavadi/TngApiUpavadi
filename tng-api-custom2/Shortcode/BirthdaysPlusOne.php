5cb9cd2016d47f3b550f698540a8c9fe15876e6a
upavadi <mahesh@upavadi.net>
2021-09-09 11:41:44 +0100

    upadate

---
+++ tng-api-custom2/Shortcode/BirthdaysPlusOne.php
@@ -0,0 +1,32 @@
+<?php
+
+class TngApiCustom_Shortcode_BirthdaysPlusOne extends Upavadi_Shortcode_AbstractCustomShortcode
+{
+    const SHORTCODE = 'tngcustom_pages_birthdaysplusone';
+
+    public function show()
+    {
+        //$this->content->init();
+
+        $month = date('m');
+        $birthdaysplusone = $this->custom->getBirthdaysPlusOne($month);
+        // Get Media
+		$url = $this->content->getTngUrl();
+		$photos = $this->content->getTngPhotoFolder();
+		$photosPath = $url. $photos;
+		//get and hold current user
+		$currentperson = $this->content->getCurrentPersonId($person['personID']);
+        $context = array(
+            'month' => $month,
+			'gedcom' => $tree,
+			'personid' => $personId,
+            'birthdaysplusone' => $birthdaysplusone,
+            'date' => date('F Y', strtotime('+1 month')),
+			'currentperson' => $currentperson,
+			'dmedia' => $dmedia,
+			'photosPath' => $photosPath,
+			'mediaPath' => $mediaPath
+        );
+        return $this->templates->render('birthdaysplusone.html', $context);
+    }
+}
